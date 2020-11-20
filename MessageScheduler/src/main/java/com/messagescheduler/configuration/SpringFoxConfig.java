52790e4abd47dc48521f007b847d942fe12235c1
Varun Choubey <varun.choubey1989@gmail.com>
2020-11-20 17:04:15 -0500

    adding swagger

---
+++ MessageScheduler/src/main/java/com/messagescheduler/configuration/SpringFoxConfig.java
@@ -0,0 +1,18 @@
+package com.messagescheduler.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {                                    
+//    @Bean
+//    public Docket api() { 
+//        return new Docket(DocumentationType.SWAGGER_2)  
+//          .select()                                  
+//          .apis(RequestHandlerSelectors.any())              
+//          .paths(PathSelectors.any())                          
+//          .build();                                           
+//    }
+}
